5fee3cead2b5a0f7bdcb4341712e4d1f9659d5a7
DarkBear0121 <darkbear0112@gmail.com>
2022-06-07 22:42:18 -0500

    Addin app.get and app.listeng

--- projects/Nodejs/myApp.js
+++ projects/Nodejs/myApp.js
@@ -1,6 +1,15 @@
 let express = require('express');
 let app = express();
 
-console.log("Hello World")
+// req = objeto de solicitud
+// res = objeto de respuesta
+
+app.get("/", (req, res) => {
+    res.send("Hello Express");
+});
+
+app.listen(3000, function() {
+    console.log("App de ejemplo, listeing in port 3000");
+});
 
 module.exports = app;
