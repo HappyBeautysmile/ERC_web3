45fc8c749773c5c857c865cd4321e170bf577e3a
DarkBear0121 <darkbear0112@gmail.com>
2022-06-09 23:06:36 -0500

    Adding smart contract to upgrade

---
+++ projects/proxy/contracts/Box.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+contract Box {
+    uint256 private value;
+
+    // Emitted when thee value has changed
+    event ValueChanged(uint256 newValue);
+
+    function store(uint256 newValue) public {
+        value = newValue;
+        emit ValueChanged(newValue);
+    }
+
+    // Reads the last stored value
+    function retrieve() public view returns (uint256) {
+        return value;
+    }
+}
