4ba7085eebadfb9d2f2d394d8cbc8d3aaf2b1046
DarkBear0121 <darkbear0112@gmail.com>
2022-08-29 22:27:52 -0500

    ERC721 deployed basics

--- projects/ERC721/contracts/ERC721.sol
+++ projects/ERC721/contracts/ERC721.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.4;
 
-contract ERC721 {}
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract NFT is ERC721 {
+    constructor() ERC721("NFT", "MEXA") {
+        _mint(msg.sender, 1);
+    }
+}
--- projects/ERC721/hardhat.config.js
+++ projects/ERC721/hardhat.config.js
@@ -1,6 +1,12 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle")
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.4",
+  networks: {
+    mumbai: {
+      url: process.env.ALCHEMY_MUMBAI_URL,
+      accounts: [process.env.PRIVATE_KEY_MUMBAI]
+    },
+  },
 };
